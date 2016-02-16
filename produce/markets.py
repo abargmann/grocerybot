30d50deaf3de35af76720652f8c05dad0da0a793
Alex Bargmann <abargmann@gmail.com>
2016-02-16 14:33:34 -0500

    error handling

--- produce/markets.py
+++ produce/markets.py
@@ -4,8 +4,13 @@ import json
 def get_markets_by_zip(zipcode):
 	response = requests.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip="
 		+ zipcode)
-	json_data = json.loads(response.text)
-	# results = json_data['results']
-	for i in json_data['results']:
-		print i['marketname']
+	if response.status_code != requests.codes.ok:
+		raise ApiError('Response: '.format(response.status_code))
+	else:
+		json_data = json.loads(response.text)
+		# results = json_data['results']
+		for i in json_data['results']:
+			print i['marketname']
 
+user_input = raw_input("Some input please: ")
+get_markets_by_zip(user_input)
